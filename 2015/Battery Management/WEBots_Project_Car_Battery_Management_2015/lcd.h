4bab09e2c16c21fc97f4516f1ddb541210cd8978
Andrew Cullen <andrew.cullen19@gmail.com>
2015-06-02 22:16:51 -0400

    Initial commit for the battery management program - Pin out definitions - Initial processor configuration - LCD header to interface with 16x2 LCD module

---
+++ 2015/Battery Management/WEBots_Project_Car_Battery_Management_2015/lcd.h
@@ -0,0 +1,137 @@
+//LCD Functions Developed by electroSome
+#include <xc.h>
+#include <stdio.h> // allows access to sprintf
+
+void LCD_Port(char a)
+{
+	if(a & 16)
+		D4 = 1;
+	else
+		D4 = 0;
+
+	if(a & 32)
+		D5 = 1;
+	else
+		D5 = 0;
+
+	if(a & 64)
+		D6 = 1;
+	else
+		D6 = 0;
+
+	if(a & 128)
+		D7 = 1;
+	else
+		D7 = 0;
+}
+
+void pulse()
+{
+    EN = 1;
+    __delay_ms(1);          // enable pule width >= 300ns
+    EN = 0;                 // clock enable -> falling edge
+}
+
+// first 4 bit number to be sent is in the high order 4 bits and the
+// second 4 bit number to be sent is in the low order 4 bits
+void LCD_Cmd(char a)
+{
+	LCD_Port(a);        // put the data on the output port
+        RS = 0;             // RS = 0, send instruction
+        //RW = 0;             // RW = 0, write
+        pulse();            // send the lower 4 bits
+        a = a<<4;           // shift to access the higher order bytes
+	LCD_Port(a);        // put the data on the output port
+        pulse();            // send the upper 4 bits
+}
+
+// 4 bit instruction to be sent is in the high order bit of a
+void LCD_Cmd_4bit(char a)
+{
+	LCD_Port(a);        // put the data on the output port
+        RS = 0;             // RS = 0, send instruction
+        //RW = 0;             // RW = 0, write
+        pulse();            // send the lower 4 bits
+}
+
+void LCD_Init()
+{
+    LCD_Port(0);
+    __delay_ms(100);         // wait for at least 15ms after power is applied
+    LCD_Cmd_4bit(0x30);      // function set
+    __delay_ms(100);
+    LCD_Cmd(0x28);           // function set
+    __delay_ms(100);
+    LCD_Cmd(0x28);           // function set
+    __delay_ms(100);
+    LCD_Cmd(0x0F);           // Display on/off control
+    __delay_ms(100);         // display on, cursor on, blink on
+    LCD_Cmd(0x01);           // Display clear
+    __delay_ms(100);
+    LCD_Cmd(0x06);           // entry mode set
+    __delay_ms(100);
+}
+
+void LCD_Write_Char(char a)
+{
+    LCD_Port(a);        // put the data on the output port
+    RS = 1;             // RS = 0, send data
+    //RW = 0;             // RW = 0, write
+    pulse();            // send the lower 4 bits
+    a = a<<4;           // shift to access the higher order bytes
+    LCD_Port(a);        // put the data on the output port
+    pulse();            // send the upper 4 bits
+}
+
+
+// for first line specify high order bits of a to 0
+// and for second line specify high order bits of a to 1
+// the location along the line is specified by a [0 - F]
+void LCD_Set_Cursor(char a )
+{
+    if (a & 16) // second line of LCD
+    {
+       LCD_Cmd_4bit(0xC0);
+       a = a << 4;
+       LCD_Cmd_4bit(a);
+    }
+
+    else        // first line of LCD
+    {
+       LCD_Cmd_4bit(0x80);
+       a = a << 4;
+       LCD_Cmd_4bit(a);
+    }
+}
+
+void LCD_Write_String(char *a)
+{
+    for(int i=0;a[i]!='\0';i++)
+    {
+       LCD_Write_Char(a[i]);
+    }
+}
+
+void LCD_Write_Int(int a)
+{
+    char s[5];
+
+    sprintf(s, "%d", a);
+    
+    LCD_Write_String(&s);
+}
+
+void LCD_Write_Float(float a)
+{
+    char s[5];
+
+    sprintf(s, "%f", a);
+
+    LCD_Write_String(&s);
+}
+
+void LCD_Clear()
+{
+    LCD_Cmd(0x20);
+    LCD_Set_Cursor(0x00);
+}
---
+++ 2015/Battery Management/WEBots_Project_Car_Battery_Management_2015/setup.h
@@ -0,0 +1,164 @@
+/************************************************************************
+*                                                                       *
+*   Filename:       setup.h                                             *
+*   Date:           02/06/2015                                          *
+*   File Version:   1.0                                                 *
+*                                                                       *
+*   Author:         Andrew Cullen                                       *
+*                                                                       *
+*************************************************************************
+*                                                                       *
+*   Architecture:   Mid-range PIC                                       *
+*   Processor:      PIC16F917                                           *
+*   Compiler:       MPLAB XC8 v1.00 (Free mode)                         *
+*                                                                       *
+*************************************************************************
+*                                                                       *
+*   Files required: none                                                *
+*                                                                       *
+*************************************************************************
+*                                                                       *
+*   Description:    This file contains declarations, definitions and    *
+*                   seting up registers for the WEBots Project Car      *
+*                   battery management program                          *
+*                                                                       *
+*************************************************************************
+*                                                                       *
+*   Pin assignments:                                                    *
+*                   C1_MEAS_UF      - AN0                               *
+*                   C2_MEAS_UF      - AN1                               *
+*                   1.982V_REF_UF   - AN2                               *
+*                   C5_MEAS_UF      - AN3                               *
+*                   TEMP_TH         - RA4                               *
+*                   C6_MEAS_UF      - AN4                               *
+*                   C3_MEAS_UF      - AN5                               *
+*                   C4_MEAS_UF      - AN6                               *
+*                   CURRENT_MEAS    - AN7                               *
+*                   CG_SEL1         - RA7                               *
+*                   CG_SEL0         - RA6                               *
+*                   MOTOR_CONTROL   - RC0                               *
+*                   R_LED           - RC1                               *
+*                   OPAMP_CGND      - RC2                               *
+*                   TP_CLS_EN       - RC3                               *
+*                   BT_CLS_EN       - RD0                               *
+*                   REF_EN          - RD1                               *
+*                   LCD_DB7         - RB5                               *
+*                   LCD_DB6         - RB4                               *
+*                   LCD_DB5         - RB3                               *
+*                   LCD_DB4         - RB2                               *
+*                   LCD_EN          - RB1                               *
+*                   LCD_RS          - RB0                               *
+*                   LCD_RW          - RD7                               *
+*                   LCD_PWR_SW      - RD6                               *
+*                   LCD_PB          - RD5                               *
+*                   G_LED           - RD4                               *
+*                   5V_REG1_EN      - RC5                               *
+*                   5V_REG2_EN      - RC4                               *
+*                   12V_REG_EN      - RD3                               *
+*                   UC_CGND         - RD2                               *
+*                                                                       *
+************************************************************************/
+
+#ifndef SETUP_H
+#define	SETUP_H
+
+/***** CONFIGURATION *****/
+/*
+WDTE =	Watchdog Timer Enable bit
+    ON      WDT enabled
+    OFF     WDT disabled and can be enabled by SWDTEN bit of the WDTCON register
+PWRTE =	Power Up Timer Enable bit
+    OFF     PWRT disabled
+    ON      PWRT enabled
+CP =	Code Protection bit
+    OFF     Program memory code protection is disabled
+    ON      Program memory code protection is enabled
+BOREN =	Brown-out Reset Selection bits
+    ON      BOR enabled
+    OFF     BOR disabled
+    NSLEEP  BOR enabled during operation and disabled in Sleep
+    SBODEN  BOR controlled by SBOREN bit of the PCON register
+DEBUG =	In-Circuit Debugger Mode bit
+    OFF     In-Circuit Debugger disabled, RB6/ISCPCLK and RB7/ICSPDAT are general purpose I/O pins
+    ON      In-Circuit Debugger enabled, RB6/ICSPCLK and RB7/ICSPDAT are dedicated to the debugger
+FCMEN =	Fail-Safe Clock Monitor Enabled bit
+    ON      Fail-Safe Clock Monitor is enabled
+    OFF     Fail-Safe Clock Monitor is disabled
+MCLRE =	RE3/MCLR pin function select bit
+    ON      RE3/MCLR pin function is MCLR
+    OFF     RE3/MCLR pin function is digital input, MCLR internally tied to VDD
+CPD =	Data Code Protection bit
+    OFF     Data memory code protection is disabled
+    ON      Data memory code protection is enabled
+IESO =	Internal External Switchover bit
+    ON      Internal/External Switchover mode is enabled
+    OFF     Internal/External Switchover mode is disabled
+FOSC =	Oscillator Selection bits
+    HS          HS oscillator: High-speed crystal/resonator on RA6/OSC2/CLKOUT/T1OSO and RA7/OSC1/CLKIN/T1OSI
+    INTOSCIO	INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT/T1OSO pin, I/O function on RA7/OSC1/CLKIN/T1OSI
+    INTOSCCLK	INTOSC oscillator: CLKOUT function on RA6/OSC2/CLKOUT/T1OSO pin, I/O function on RA7/OSC1/CLKIN/T1OSI
+    LP          LP oscillator: Low-power crystal on RA6/OSC2/CLKOUT/T1OSO and RA7/OSC1/CLKIN/T1OSI
+    EXTRCIO	RCIO oscillator: I/O function on RA6/OSC2/CLKOUT/T1OSO pin, RC on RA7/OSC1/CLKIN/T1OSI
+    EC          EC: I/O function on RA6/OSC2/CLKOUT/T1OSO pin, CLKIN on RA7/OSC1/CLKIN/T1OSI
+    XT          XT oscillator: Crystal/resonator on RA6/OSC2/CLKOUT/T1OSO and RA7/OSC1/CLKIN/T1OSI
+    EXTRCCLK	RC oscillator: CLKOUT function on RA6/OSC2/CLKOUT/T1OSO pin, RC on RA7/OSC1/CLKIN/T1OSI
+ */
+
+#pragma config WDTE = OFF, PWRTE = OFF, CP = OFF, BOREN = OFF, DEBUG = OFF, FCMEN = OFF, MCLRE = ON, CPD = OFF, IESO = ON, FOSC = INTOSCIO
+
+#include <xc.h>
+#include <stdint.h> // allows access to more intuiative interger classes such as uint8_t, int16_t
+
+/***** LCD Definitions *****/
+
+#define D7 RB5
+#define D6 RB4
+#define D5 RB3
+#define D4 RB2
+#define EN RB1
+#define RS RB0
+#define RW RD7
+#define LCD_PWR_SW RD6
+#define LCD_PB RD5
+
+#define _XTAL_FREQ 4000000  // used for _delay function, specifies a clock frequency of 4MHz
+
+#include "lcd.h" // header file to interface with 16x2 LCD screen
+
+/***** Cell Measurement Definitions *****/
+
+#define REF AN2
+#define C1_MEAS AN0
+#define C2_MEAS AN1
+#define C3_MEAS AN5
+#define C4_MEAS AN6
+#define C5_MEAS AN3
+#define C6_MEAS AN4
+#define REF_EN RD1
+#define TP_CLS_EN RC3
+#define BT_CLS_EN RD0
+#define OPAMP_CGND RC2 
+
+/***** Current Measurement Definitions *****/
+
+#define CURRENT_MEAS AN7
+#define CG_SEL1 RA7
+#define CG_SEL0 RA6
+
+/***** Shutoff/Enable Definitions *****/
+
+#define UC_CGND RD2
+#define MOTOR_CONTROL RC0
+#define REG1_5V_EN RC5
+#define REG2_5V_EN RC4
+#define REG_12V_EN RD3
+#define TEMP_TH RA4
+
+/***** LED Definitions *****/
+
+#define R_LED RC1
+#define G_LED RD4
+
+
+#endif	/* SETUP_H */
+
