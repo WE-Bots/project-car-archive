3c3fa57185df1471ce001411f340b479f5181abb
Kevin McLean <kmclea52@uwo.ca>
2016-05-15 13:37:48 -0400

    Initial commit for the CAN to USB transceiver.

---
+++ 2016/CAN_to_USB_transceiver.X/CAN.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   CAN.h
+ * Author: Kevin
+ *
+ * Created on May 15, 2016, 12:37 PM
+ */
+
+#ifndef CAN_H
+#define	CAN_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CAN_H */
+
---
+++ 2016/CAN_to_USB_transceiver.X/main.c
@@ -0,0 +1,7 @@
+#include <CAN.h>
+
+void main()
+{
+    
+}
+
