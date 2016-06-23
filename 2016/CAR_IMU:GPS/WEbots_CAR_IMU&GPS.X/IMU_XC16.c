06f90991972de41a189dac93bcc55e40f8f384f6
Patrick Egan <PatrickEgan1996@gmail.com>
2016-06-22 20:42:49 -0400

    Added MPLAB IMU project

---
+++ 2016/CAR_IMU:GPS/WEbots_CAR_IMU&GPS.X/IMU_XC16.c
@@ -0,0 +1,37 @@
+/*
+ * File:   IMU_XC16.c
+ * Author: patrick egan
+ *
+ * Created on June 21, 2016, 8:12 PM
+ */
+
+/* List of extra TODO's
+ * - Check for whether there will be a LED on the PCB
+ * - Figure out which ports are being used to send and receive data
+ * - Figure out where to store Quaternion data
+ * - Write a simple api for the Keiman Filter
+ * - Write and research calibration code
+ */
+
+/**************************************
+ * TODO: Complete layout of memory addresses with descriptions
+ * of what each thing does at its respective address
+ * 
+ * MEMORY STRUCTURE:
+ * 
+ *************************************/
+
+#include "xc.h"
+
+//TODO: Setup pre-processor directives for defining macros which
+//      will be useful later
+
+int main(void) {
+    return 0;
+    
+}
+
+//TODO: Setup global variables for ports used to send/receive data
+//      and memory addresses used for saving quaternion data
+
+//TODO: 
