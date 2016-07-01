e7ea16c052bd173ece1a8debf3d078e805480d90
Kevin McLean <kmclea52@uwo.ca>
2016-06-30 23:29:40 -0400

    Added code to test the receive function of the UART.

--- 2016/CAN_to_USB_transceiver.X/main.c
+++ 2016/CAN_to_USB_transceiver.X/main.c
@@ -10,22 +10,36 @@ void main()
     CANTransmit(0, 0, 0);
      */
     unsigned long i = 0;
+    char o = 0;
     TRISDbits.TRISD2 = 0;
     UARTInit();
     while (1)
     {
-        LATDbits.LATD2 = 1;
-        if (!BusyUART1())
+        if (DataRdyUART1())
         {
-            WriteUART1('1');
-        }
-        for (i = 0; i < 0xFFFF; i++)
-        {
-        }
-        LATDbits.LATD2 = 0;
-        if (!BusyUART1())
-        {
-            WriteUART1('0');
+            o = ReadUART1();
+            switch (o)
+            {
+                case '1':
+                    if (!BusyUART1())
+                    {
+                        putsUART1("1\n");
+                        LATDbits.LATD2 = 1;
+                    }
+                    break;
+                case '0':
+                    if (!BusyUART1())
+                    {
+                        putsUART1("0\n");
+                        LATDbits.LATD2 = 0;
+                    }
+                    break;
+                default:
+                    putsUART1("Unknown: ");
+                    putsUART1(o);
+                    putsUART1("\n");
+                    break;
+            }
         }
         for (i = 0; i < 0xFFFF; i++)
         {
