8740276fc60871a45ea2dfd4a33b0f46fd3405b1
Patrick Egan <PatrickEgan1996@gmail.com>
2016-07-06 22:35:48 -0400

    Just a little cleaning up

--- 2016/WEbots_CAR_IMU&GPS.X/MPU9250.c
+++ 2016/WEbots_CAR_IMU&GPS.X/MPU9250.c
@@ -138,7 +138,7 @@ bool MPU9250::init(bool calib_gyro, bool calib_acc){
  * returns the range set (2,4,8 or 16)
  */
 
-unsigned int MPU9250::set_acc_scale(int scale){
+unsigned int set_acc_scale(int scale){
     unsigned int temp_scale;
     WriteReg(MPUREG_ACCEL_CONFIG, scale);
 
@@ -187,7 +187,7 @@ unsigned int MPU9250::set_acc_scale(int scale){
  * returns the range set (250,500,1000 or 2000)
  */
 
-unsigned int MPU9250::set_gyro_scale(int scale){
+unsigned int set_gyro_scale(int scale){
     unsigned int temp_scale;
     WriteReg(MPUREG_GYRO_CONFIG, scale);
 
@@ -216,7 +216,7 @@ unsigned int MPU9250::set_gyro_scale(int scale){
  * mpu9250 which should be 0x71
  */
 
-unsigned int MPU9250::whoami(){
+unsigned int whoami(){
     unsigned int response;
     response = WriteReg(MPUREG_WHOAMI|READ_FLAG, 0x00);
     return response;
@@ -231,7 +231,7 @@ unsigned int MPU9250::whoami(){
  * 2 -> Z axis
  */
 
-void MPU9250::read_acc()
+void read_acc()
 {
     unsigned char response[6];
     signed int bit_data;
@@ -253,7 +253,7 @@ void MPU9250::read_acc()
  * 2 -> Z axis
  */
 
-void MPU9250::read_gyro()
+void read_gyro()
 {
     unsigned char response[6];
     signed int bit_data;
@@ -274,7 +274,7 @@ void MPU9250::read_gyro()
  * returns the value in °C
  */
 
-void MPU9250::read_temp(){
+void read_temp(){
     unsigned char response[2];
     signed int bit_data;
     float data;
@@ -294,7 +294,7 @@ void MPU9250::read_temp(){
  * returns Factory Trim value
  */
 
-void MPU9250::calib_acc()
+void calib_acc()
 {
     unsigned char response[4];
     int temp_scale;
@@ -312,7 +312,7 @@ void MPU9250::calib_acc()
     set_acc_scale(temp_scale);
 }
 
-unsigned char MPU9250::AK8963_whoami(){
+unsigned char AK8963_whoami(){
     unsigned char response;
     WriteReg(MPUREG_I2C_SLV0_ADDR,AK8963_I2C_ADDR|READ_FLAG); //Set the I2C slave addres of AK8963 and set for read.
     WriteReg(MPUREG_I2C_SLV0_REG, AK8963_WIA); //I2C slave 0 register address from where to begin data transfer
@@ -327,7 +327,7 @@ unsigned char MPU9250::AK8963_whoami(){
     return response;
 }
 
-void MPU9250::calib_mag(){
+void calib_mag(){
     unsigned char response[3];
     float data;
     int i;
@@ -347,7 +347,7 @@ void MPU9250::calib_mag(){
         Magnetometer_ASA[i] = ((data-128)/256+1)*Magnetometer_Sensitivity_Scale_Factor;
     }
 }
-void MPU9250::read_mag(){
+void read_mag(){
     unsigned char response[7];
     float data;
     int i;
@@ -366,7 +366,7 @@ void MPU9250::read_mag(){
     }
 }
 
-unsigned char MPU9250::get_CNTL1(){
+unsigned char get_CNTL1(){
     WriteReg(MPUREG_I2C_SLV0_ADDR,AK8963_I2C_ADDR|READ_FLAG); //Set the I2C slave addres of AK8963 and set for read.
     WriteReg(MPUREG_I2C_SLV0_REG, AK8963_CNTL1); //I2C slave 0 register address from where to begin data transfer
     WriteReg(MPUREG_I2C_SLV0_CTRL, 0x81); //Read 1 byte from the magnetometer
@@ -375,7 +375,7 @@ unsigned char MPU9250::get_CNTL1(){
     return WriteReg(MPUREG_EXT_SENS_DATA_00|READ_FLAG, 0x00);    //Read I2C
 }
 
-void MPU9250::read_all(){
+void read_all(){
     unsigned char response[21];
     signed int bit_data;
     float data;
@@ -412,10 +412,10 @@ void MPU9250::read_all(){
     }
 }
 
-void MPU9250::calibrate(float *dest1, float *dest2){
+void calibrate(float *dest1, float *dest2){
     unsigned char data[12]; // data array to hold accelerometer and gyro x, y, z, data
     usigned int ii, packet_count, fifo_count;
-    int32_t gyro_bias[3]  = {0, 0, 0}, accel_bias[3] = {0, 0, 0};
+    signed long gyro_bias[3]  = {0, 0, 0}, accel_bias[3] = {0, 0, 0};
 
     // reset device
     WriteReg(MPUREG_PWR_MGMT_1, 0x80); // Write a one to bit 7 reset bit; toggle reset device
@@ -466,23 +466,23 @@ void MPU9250::calibrate(float *dest1, float *dest2){
         gyro_temp[1]  = (signed int) (((signed int)data[8] << 8) | data[9]  ) ;
         gyro_temp[2]  = (signed int) (((signed int)data[10] << 8) | data[11]) ;
 
-        accel_bias[0] += (int32_t) accel_temp[0]; // Sum individual signed 16-bit biases to get accumulated signed 32-bit biases
-        accel_bias[1] += (int32_t) accel_temp[1];
-        accel_bias[2] += (int32_t) accel_temp[2];
-        gyro_bias[0]  += (int32_t) gyro_temp[0];
-        gyro_bias[1]  += (int32_t) gyro_temp[1];
-        gyro_bias[2]  += (int32_t) gyro_temp[2];
+        accel_bias[0] += (signed long) accel_temp[0]; // Sum individual signed 16-bit biases to get accumulated signed 32-bit biases
+        accel_bias[1] += (signed long) accel_temp[1];
+        accel_bias[2] += (signed long) accel_temp[2];
+        gyro_bias[0]  += (signed long) gyro_temp[0];
+        gyro_bias[1]  += (signed long) gyro_temp[1];
+        gyro_bias[2]  += (signed long) gyro_temp[2];
 
     }
-    accel_bias[0] /= (int32_t) packet_count; // Normalize sums to get average count biases
-    accel_bias[1] /= (int32_t) packet_count;
-    accel_bias[2] /= (int32_t) packet_count;
-    gyro_bias[0]  /= (int32_t) packet_count;
-    gyro_bias[1]  /= (int32_t) packet_count;
-    gyro_bias[2]  /= (int32_t) packet_count;
+    accel_bias[0] /= (signed long) packet_count; // Normalize sums to get average count biases
+    accel_bias[1] /= (signed long) packet_count;
+    accel_bias[2] /= (signed long) packet_count;
+    gyro_bias[0]  /= (signed long) packet_count;
+    gyro_bias[1]  /= (signed long) packet_count;
+    gyro_bias[2]  /= (signed long) packet_count;
 
-    if(accel_bias[2] > 0L) {accel_bias[2] -= (int32_t) accelsensitivity;}  // Remove gravity from the z-axis accelerometer bias calculation
-    else {accel_bias[2] += (int32_t) accelsensitivity;}
+    if(accel_bias[2] > 0L) {accel_bias[2] -= (signed long) accelsensitivity;}  // Remove gravity from the z-axis accelerometer bias calculation
+    else {accel_bias[2] += (signed long) accelsensitivity;}
 
     // Construct the gyro biases for push to the hardware gyro bias registers, which are reset to zero upon device startup
     data[0] = (-gyro_bias[0]/4  >> 8) & 0xFF; // Divide by 4 to get 32.9 LSB per deg/s to conform to expected bias input format
@@ -511,15 +511,15 @@ void MPU9250::calibrate(float *dest1, float *dest2){
     // compensation calculations. Accelerometer bias registers expect bias input as 2048 LSB per g, so that
     // the accelerometer biases calculated above must be divided by 8.
 
-    int32_t accel_bias_reg[3] = {0, 0, 0}; // A place to hold the factory accelerometer trim biases
+    signed long accel_bias_reg[3] = {0, 0, 0}; // A place to hold the factory accelerometer trim biases
     ReadRegs(MPUREG_XA_OFFSET_H, data, 2); // Read factory accelerometer trim values
-    accel_bias_reg[0] = (int32_t) (((signed int)data[0] << 8) | data[1]);
+    accel_bias_reg[0] = (signed long) (((signed int)data[0] << 8) | data[1]);
     ReadRegs(MPUREG_YA_OFFSET_H, data, 2);
-    accel_bias_reg[1] = (int32_t) (((signed int)data[0] << 8) | data[1]);
+    accel_bias_reg[1] = (signed long) (((signed int)data[0] << 8) | data[1]);
     ReadRegs(MPUREG_ZA_OFFSET_H, data, 2);
-    accel_bias_reg[2] = (int32_t) (((signed int)data[0] << 8) | data[1]);
+    accel_bias_reg[2] = (signed long) (((signed int)data[0] << 8) | data[1]);
 
-    uint32_t mask = 1uL; // Define mask for temperature compensation bit 0 of lower byte of accelerometer bias registers
+    unsigned long mask = 1uL; // Define mask for temperature compensation bit 0 of lower byte of accelerometer bias registers
     unsigned char mask_bit[3] = {0, 0, 0}; // Define array to hold mask bit for each accelerometer bias axis
 
     for(ii = 0; ii < 3; ii++) {
@@ -567,7 +567,7 @@ void select() {
     digitalWrite(my_cs, LOW);
 #endif
 }
-void MPU9250::deselect() {
+void deselect() {
     //Set CS high to stop transmission (restarts conversion)
 #ifdef CORE_TEENSY
     digitalWriteFast(my_cs, HIGH);
